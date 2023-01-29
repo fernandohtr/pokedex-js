95ef7a7aefe02aa90b8f036723dd513e851d955a
Fernando Toledo <fernandohtr@gmail.com>
2023-01-29 11:39:28 -0300

    Add fetch for 151 pokemons

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const fetchPokemon = async () => {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon/7');
-  const pokemonData = await response.json();
-  console.log(pokemonData);
+  const getPokemonById = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+  const pokemonPromises = [];
+  const pokemonNumbers = 151;
+  let response;
+
+  for (let i = 1; i <= pokemonNumbers; i++) {
+    response = await fetch(getPokemonById(i));
+    pokemonPromises.push(response.json());
+  }
+  let promise = await Promise.all(pokemonPromises);
+  console.log(promise);
 }
 
 fetchPokemon();
