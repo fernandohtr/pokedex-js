7c62bf38806cd196c30e99acad3921fb5ac6f7f2
Fernando Toledo <fernandohtr@gmail.com>
2023-01-30 00:29:39 -0300

    Add html element builder

--- src/app.js
+++ src/app.js
@@ -1,15 +1,39 @@
-const fetchPokemon = async () => {
-  const getPokemonById = id => `https://pokeapi.co/api/v2/pokemon/${id}`
-  const pokemonPromises = [];
+
+async function fetchPokemon() {
+  const pokemonsPromises = [];
   const pokemonNumbers = 151;
   let response;
 
   for (let i = 1; i <= pokemonNumbers; i++) {
     response = await fetch(getPokemonById(i));
-    pokemonPromises.push(response.json());
+    pokemonsPromises.push(response.json());
   }
-  let promise = await Promise.all(pokemonPromises);
-  console.log(promise);
+  await Promise.all(pokemonsPromises)
+    .then(pokemons => {
+
+      const pokemonList = pokemons.reduce((element, pokemon) => {
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+
+        element += `
+          <li class="card ${types[0]}">
+            <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${zeroPadding(pokemon.id)}.png">
+            <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+            <p class="card-subtitle">${types.join(' | ')}</p>
+          </li>
+        `;
+        return element;
+      }, '');
+      const ul = document.querySelector('[data-js="pokedex"]');
+      ul.innerHTML = pokemonList;
+    });
+};
+
+function getPokemonById(id) {
+  return `https://pokeapi.co/api/v2/pokemon/${id}`
+}
+
+function zeroPadding(number) {
+  return String(number).padStart(3, '0');
 }
 
 fetchPokemon();
